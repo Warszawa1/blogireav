a20dac6771a4dec63e16d66f58b580049aad8bb0
Irene Antelo <iantelovazquez@gmail.com>
2024-09-04 22:27:29 +0200

    Fix admin issue

--- myblog/urls.py
+++ myblog/urls.py
@@ -22,7 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # This includes URLs from the blog app
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
