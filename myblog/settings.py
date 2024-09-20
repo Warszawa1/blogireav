e9bb55e2e697afe92ef017f62fecc753fa1e4347
Irene Antelo <iantelovazquez@gmail.com>
2024-09-20 10:44:29 +0200

    Fixed static files configuration

--- myblog/settings.py
+++ myblog/settings.py
@@ -167,8 +167,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog', 'static'),
-    os.path.join(BASE_DIR, 'theme', 'static'),
+    os.path.join(BASE_DIR, 'myblog', 'static'),
 ]
 
 # Add this for WhiteNoise
