cb8133f53a82177c9994ea80219d349ff06f1fa2
Irene Antelo <iantelovazquez@gmail.com>
2024-09-27 17:58:30 +0200

    change to ImageField the image in postform

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    image = models.URLField(editable=True, blank=True, null=True)
+    image = models.ImageField(upload_to='post_images/', editable=True, blank=True, null=True)
+
 
     def __str__(self):
         return self.title
--- myblog/urls.py
+++ myblog/urls.py
@@ -23,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # This includes URLs from the blog app
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
